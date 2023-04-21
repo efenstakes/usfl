f9d68f93a7f3486ececfa44de82cee0057c420d6
efen stakes <efenstakes101@gmail.com>
2023-04-21 13:41:30 +0300

    add 3d football component

---
+++ src/components/football.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/models/Football.glb");
+  
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Football_Sphere002_1.geometry}
+        material={materials["795548"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Football_Sphere002_1_1.geometry}
+        material={materials.FFFFFF}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/models/Football.glb");
