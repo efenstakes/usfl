6606a0cdc61eb5bd1794698e11cf7d86a5954f34
efen stakes <efenstakes101@gmail.com>
2023-04-21 13:39:17 +0300

    add 3d helmet component

---
+++ src/components/helmet.jsx
@@ -0,0 +1,67 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/Football helmet.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.main_helmet.geometry}
+        material={materials.Mat}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.back_soft.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Top_soft.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Front_soft.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Back_neck.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.side.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Top_strip.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Face_guard_holder.geometry}
+        material={materials["default"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Sweep.geometry}
+        material={materials.White}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/models/Football helmet.glb");
+
